d7a424c8def97cb6e70a4975b14498e57a0b24ef
Tahoe Schrader <tahoeschrader@gmail.com>
2017-03-19 21:40:15 -0400

    Possible diffusion solver completion and latex update
    Sigma does not exactly match the analytic solution so there may still be
    a bug. I updated the latex with a writeup on this code.

--- PHY566_group4_project#1a_diffusionequation.py
+++ PHY566_group4_project#1a_diffusionequation.py
@@ -5,8 +5,8 @@
 ################################################################################
 ### This code will look at the 1D diffusion equation:
 ###       a) Analytically, we show that the spatial expectation value of the 1D
-###          normal distribution equals sigma^2. WILL BE DERIVED IN LATEX PAPER
-###             --- CURRENTLY INCOMPLETE
+###          normal distribution equals sigma^2.
+###             --- COMPLETE
 ###       b) Solve the 1D diff. eq. using the finite difference form and a
 ###          diffusion constant of D=2.
 ###             --- CURRENTLY INCOMPLETE
@@ -32,63 +32,172 @@ from lmfit import Model                 # Curve fitting package
 
 # ------------------------------------------------------------------------------
 
-diffconst = 2.0                 # diffusion constant
-xsteps = .1                     # an xdimension stepsize <1 is ideal
-xlength = 100
-peak = 100.0                    # this is where the box density sharply peaks
-tsteps = 1
-time = 100
+diffconst = 2.0                       # diffusion constant (given)
+xsteps = .05                          # a dx<1 is ideal for smoothness
+xlength = 5
+peak = 100                            # original box shaped density peak
+tsteps = xsteps**2/(2*diffconst)/2.0  # guarantees stability if dt <= dx^2/(2D)
+                                      # so I made it .5 * dx^2/(2D) D !!!
+duration = 10
 
 # ------------------------------------------------------------------------------
 
 ################################################################################
-### Define the diffusion equation solver
+### Define the diffusion equation solver and Gaussian model
 ################################################################################
-def diffusion1D() :
-    # Initialize the density vector and xdimension to solve over
-    xlocation = np.arange(0,xlength,xsteps)
-    xiters = int(len(xlocation))
-    titers = int(time/tsteps)
-    density = np.zeros((xiters,titers))
 
-    # Define the initial box density to be sharply peaked over a few sites in x
-    # for the first time solve. I fix the last boundary condition to zero
-    density[0:10,0] = peak
-    density[xiters - 1,:] = 0.0   # xiters-1 is the final index
+# ------------------------------------------------------------------------------
+
+def diffusion1D() :
+    # Initialize the density and location vectors (in time and x)
+    xlocation = np.arange(0,xlength,xsteps)
+    tlocation = np.arange(0,duration,tsteps)
+    x_iters = int(len(xlocation))               # total x iterations
+    t_iters = int(len(tlocation))               # total t iterations
+    density = np.zeros((x_iters,t_iters))       # size of all x and t
+
+    # Define the boundary conditions
+    # For t = 0, the first couple xlocations should be the peak size
+    density[0:5,0] = peak
 
     # Iteratively solve the density equation
-    for n in range(titers - 1) :
-        for i in range(xiters - 2) : # Already fixed xiters-1 to be zero
-            density[i,n+1] = density[i,n] + diffconst * tsteps / (xsteps**2) * (density[i+1,n] + density[i-1,n] - 2 * density[i,n])
+    for t in range(t_iters - 1) :
+        for x in range(x_iters - 2) :
+            density[x,t+1] = density[x,t] + (diffconst * (tsteps/xsteps**2) * (density[x+1,t] + density[x-1,t] - 2.0 * density[x,t]))
 
     return xlocation, density
+
+# ------------------------------------------------------------------------------
+
+def Gaussian(x,sigma,amplitude,mu):
+    return amplitude * (1.0 / (sigma*np.sqrt(2*np.pi))) * np.exp(-((x-mu)**2/(2*sigma**2)))
 
 # ------------------------------------------------------------------------------
 
 ################################################################################
 ### Now plot the solutions
 ################################################################################
+
 # ------------------------------------------------------------------------------
 
+# Call the function
+location,probability = diffusion1D()
+
+# ------------------------------------------------------------------------------
 
 #                             # BEGIN PLOT ONE
-# This plot will look at a time snapshot of the diffusion equation
-xlocation,density = diffusion1D()
-print density
+# This plot will look at the initial time snapshot of the diffusion equation
 
 # Now plot
 fig = plt.figure()
-fig.suptitle('Time Snapshot of $1D$ Diffusion Equation',fontsize=25)
+fig.suptitle('Initial Box Density',fontsize=25)
 
 plt.subplot(1,1,1)
-plt.plot(xlocation,density[:,1],'b-',label='probability density')
-plt.ylabel('probability density',fontsize=20)
+plt.plot(location,probability[:,0],'b-',label='distribution')
+plt.ylabel('density',fontsize=20)
 plt.xlabel('$x$ location',fontsize=20)
 plt.legend()
 plt.grid()
+plt.axis([0,5,0,110])
 
-#plt.savefig("LaTeX\probdensity0.png")
+plt.savefig("LaTeX\probdensityinit.png")
 
 # Makes my figures show up
 plt.show()
+
+# ------------------------------------------------------------------------------
+
+#                             # BEGIN PLOT TWO-SIX
+# This plot will look at 5 snapshots in time of the diffusion solver and fit a
+# gaussian to them. First, define points in time wrt timesteps
+t1 = 10
+t2 = 50
+t3 = 500
+t4 = 1000
+t5 = 1600
+
+# Calculate the time where we are taking snapshots
+snapshot1 = tsteps * t1
+snapshot2 = tsteps * t2
+snapshot3 = tsteps * t3
+snapshot4 = tsteps * t4
+snapshot5 = tsteps * t5
+
+# To fit the gaussian, I build the gaussian Model
+gaussianmodel = Model(Gaussian)
+fitresult1 = gaussianmodel.fit(probability[:,t1], x = location , amplitude = peak , sigma = diffconst , mu = np.sqrt(2*diffconst*snapshot1))
+fitresult2 = gaussianmodel.fit(probability[:,t2], x = location , amplitude = peak , sigma = diffconst , mu = np.sqrt(2*diffconst*snapshot2))
+fitresult3 = gaussianmodel.fit(probability[:,t3], x = location , amplitude = peak , sigma = diffconst , mu = np.sqrt(2*diffconst*snapshot3))
+fitresult4 = gaussianmodel.fit(probability[:,t4], x = location , amplitude = peak , sigma = diffconst , mu = np.sqrt(2*diffconst*snapshot4))
+fitresult5 = gaussianmodel.fit(probability[:,t5], x = location , amplitude = peak , sigma = diffconst , mu = np.sqrt(2*diffconst*snapshot5))
+
+# Now I extract the best fit sigma's
+sigma1 = fitresult1.best_values['sigma']
+sigma2 = fitresult2.best_values['sigma']
+sigma3 = fitresult3.best_values['sigma']
+sigma4 = fitresult4.best_values['sigma']
+sigma5 = fitresult5.best_values['sigma']
+
+# Now develop plots
+
+fig = plt.figure()
+fig.suptitle('$1D$ Diffusion at $t=$%g w/ $\sigma=$%.2f'%(snapshot1,np.sqrt(2.0*diffconst*snapshot1)),fontsize=25)
+plt.subplot(1,1,1)
+plt.plot(location,probability[:,t1],'b-',label='distribution')
+plt.plot(location, fitresult1.best_fit,'k--', linewidth = 3, label='best fit, $\sigma = $%.2f' %sigma1)
+plt.ylabel('density',fontsize=20)
+plt.xlabel('$x$ location',fontsize=20)
+plt.legend()
+plt.grid()
+plt.savefig("LaTeX\probdensityt1.png")
+plt.show()
+
+fig = plt.figure()
+fig.suptitle('$1D$ Diffusion at $t=$%g w/ $\sigma=$%.2f' %(snapshot2,np.sqrt(2.0*diffconst*snapshot2)),fontsize=25)
+plt.subplot(1,1,1)
+plt.plot(location,probability[:,t2],'b-',label='distribution')
+plt.plot(location, fitresult2.best_fit,'k--', linewidth = 3, label='best fit, $\sigma = $%.2f' %sigma2)
+plt.ylabel('density',fontsize=20)
+plt.xlabel('$x$ location',fontsize=20)
+plt.legend()
+plt.grid()
+plt.savefig("LaTeX\probdensityt2.png")
+plt.show()
+
+fig = plt.figure()
+fig.suptitle('$1D$ Diffusion at $t=$%g w/ $\sigma=$%.2f' %(snapshot3,np.sqrt(2.0*diffconst*snapshot3)),fontsize=25)
+plt.subplot(1,1,1)
+plt.plot(location,probability[:,t3],'b-',label='distribution')
+plt.plot(location, fitresult3.best_fit,'k--', linewidth = 3, label='best fit, $\sigma = $%.2f' %sigma3)
+plt.ylabel('density',fontsize=20)
+plt.xlabel('$x$ location',fontsize=20)
+plt.legend()
+plt.grid()
+plt.savefig("LaTeX\probdensityt3.png")
+plt.show()
+
+fig = plt.figure()
+fig.suptitle('$1D$ Diffusion at $t=$%g w/ $\sigma=$%.2f' %(snapshot4,np.sqrt(2.0*diffconst*snapshot4)),fontsize=25)
+plt.subplot(1,1,1)
+plt.plot(location,probability[:,t4],'b-',label='distribution')
+plt.plot(location, fitresult4.best_fit,'k--', linewidth = 3, label='best fit, $\sigma = $%.2f' %sigma4)
+plt.ylabel('density',fontsize=20)
+plt.xlabel('$x$ location',fontsize=20)
+plt.legend()
+plt.grid()
+plt.savefig("LaTeX\probdensityt4.png")
+plt.show()
+
+fig = plt.figure()
+fig.suptitle('$1D$ Diffusion at $t=$%g w/ $\sigma=$%.2f'%(snapshot5,np.sqrt(2.0*diffconst*snapshot5)),fontsize=25)
+plt.subplot(1,1,1)
+plt.plot(location,probability[:,t5],'b-',label='distribution')
+plt.plot(location, fitresult5.best_fit,'k--', linewidth = 3, label='best fit, $\sigma = $%.2f' %sigma5)
+plt.ylabel('density',fontsize=20)
+plt.xlabel('$x$ location',fontsize=20)
+plt.legend()
+plt.grid()
+plt.savefig("LaTeX\probdensityt5.png")
+plt.show()
+
 # INCOMPLETE
