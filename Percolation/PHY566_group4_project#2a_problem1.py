669802f5ce5f85411b7e877b513811d87ddfafee
Tahoe Schrader <tahoeschrader@gmail.com>
2017-04-02 13:14:38 -0400

    Added code descriptions
    Added an outline of what to do at the top of each python file. I find
    this to help with workflow.

--- Percolation/PHY566_group4_project#2a_problem1.py
+++ Percolation/PHY566_group4_project#2a_problem1.py
@@ -3,5 +3,12 @@
 ### Spring, 2017
 
 ################################################################################
-### This code will...
+### This code will simulate the percolation transition on a N x N lattice by
+### randomly filling lattice sites. Occupation probability is a fraction of all
+### occupied sites among lattice sites in general. Therefore, each population
+### step wil increase p. We want this code to:
+###             a) Determine critical probability (p when spanning cluster occurs)
+###             b) Determine pc and plot the cluster for N = 5, 10, 15, 20, 30,
+###                50, and 80. Each of these calculations should be averaged 50x
+###             c) Plot pc(N^-1) to extrapolate infinite size limit pc(0)
 ################################################################################
